4be69071541c1d6246f673d7dbe54050a18e8aa1
Dylan Ross <dylanross620@gmail.com>
2020-05-20 20:35:52 -0400

    Removed target directory

--- target/rls/debug/deps/matrix_calculator-763ff35358e8bcc7.d
+++
@@ -1,6 +0,0 @@
-/home/dylan/Documents/projects/matrix-calculator/target/rls/debug/deps/matrix_calculator-763ff35358e8bcc7.rmeta: src/main.rs src/mat.rs
-
-/home/dylan/Documents/projects/matrix-calculator/target/rls/debug/deps/matrix_calculator-763ff35358e8bcc7.d: src/main.rs src/mat.rs
-
-src/main.rs:
-src/mat.rs:
--- target/rls/debug/deps/matrix_calculator-f2c45aa314bb956a.d
+++
@@ -1,6 +0,0 @@
-/home/dylan/Documents/projects/matrix-calculator/target/rls/debug/deps/matrix_calculator-f2c45aa314bb956a.rmeta: src/main.rs src/mat.rs
-
-/home/dylan/Documents/projects/matrix-calculator/target/rls/debug/deps/matrix_calculator-f2c45aa314bb956a.d: src/main.rs src/mat.rs
-
-src/main.rs:
-src/mat.rs:
